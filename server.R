4aa9a1ad891f97e48e6dc0d97679641bd6504bfd
eagle-eye33 <kotuanhui@hotmail.com>
2016-01-31 19:36:21 +0800

    confirmed version

--- server.R
+++ server.R
@@ -10,14 +10,17 @@ colors = c("red", "yellow", "green", "violet", "blue")
 # Define a server for the Shiny app
 shinyServer(function(input, output) {
   
+  ##Total Number of Death for this population group
+  output$prediction <- renderPrint({sum(VADeaths[,input$population_group]*1000)})
+  
   # Fill in the spot we created for a plot
   output$deathPlot <- renderPlot({
-    
-    # Render a barplot
-    barplot(VADeaths[,input$ population_group]*1000, 
-            main=input$ population_group,
-            ylab="Number of Death",
+      # Render a barplot
+    barplot(VADeaths[,input$population_group], 
+            main=input$population_group,
+            ylab="Death Rate per 1000 population",
             xlab="Age Group",
             col=colors)
   })
+  
 })
--- ui.R
+++ ui.R
@@ -11,23 +11,30 @@ shinyUI(
   fluidPage(    
     
     # Give the page a title
-    titlePanel("Number of Death in Virginia in Year 1940"),
+    titlePanel("Death Rate Analysis for in Virginia (Year 1940), sorted by Population Group"),
     
     # Generate a row with a sidebar
     sidebarLayout(      
       
       # Define the sidebar with one input
       sidebarPanel(
-        selectInput("population_group", "Population Group:", 
+        selectInput("population_group", "Please select one Population Group:", 
                     choices=colnames(VADeaths)),
         hr(),
-        helpText("This shiny app will show Number of Death in Virginia in Year 1940.  Please select the desired population group, 
-                 it will show correnspoding number of death sorted by age group")
+        helpText("This shiny app will show Death Rates in Virginia in Year 1940 on selected population group.  
+                 Please select the desired population group, 
+                 it will show correnspoding Dealth Rates sorted by age group,
+                 as well as give you the total number of death for the selection 
+                 population group"),
+        hr(),
+        helpText("Source code ui.R & server.R stored in below link https://github.com/eagle-eye33/DevelopingDataProduct")
       ),
       
       # Create a spot for the barplot
       mainPanel(
-        plotOutput("deathPlot")  
+        plotOutput("deathPlot"),
+        h3('Yearly Number of Death for this population group'),
+        verbatimTextOutput("prediction")
       )
       
     )
